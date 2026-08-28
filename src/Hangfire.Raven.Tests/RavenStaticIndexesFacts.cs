13838cd39bc975758b3374ad48cf6e6b85a8cec2
Kelvin Mateus Axhcar de Jesus <kelvin.jesus@inventsoftware.com.br>
2026-08-28 09:17:05 -0300

    test: adicionar testes unitarios para indices estaticos JobQueue e RavenJob

---
+++ src/Hangfire.Raven.Tests/RavenStaticIndexesFacts.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using Hangfire.Raven.Indexes;
+using Hangfire.Raven.Storage;
+using Moq;
+using Raven.Client.Documents.Indexes;
+using Xunit;
+
+namespace Hangfire.Raven.Tests
+{
+    public class RavenStaticIndexesFacts
+    {
+        [Fact]
+        public void JobQueue_ByQueueAndFetchedAt_HasCorrectNameAndMap()
+        {
+            var index = new JobQueue_ByQueueAndFetchedAt();
+            var definition = index.CreateIndexDefinition();
+
+            Assert.Equal("JobQueue/ByQueueAndFetchedAt", index.IndexName);
+            Assert.NotEmpty(definition.Maps);
+        }
+
+        [Fact]
+        public void RavenJobs_ByStateAndCreatedAt_HasCorrectNameAndMap()
+        {
+            var index = new RavenJobs_ByStateAndCreatedAt();
+            var definition = index.CreateIndexDefinition();
+
+            Assert.Equal("RavenJobs/ByStateAndCreatedAt", index.IndexName);
+            Assert.NotEmpty(definition.Maps);
+        }
+
+        [Fact]
+        public void RavenStorage_Ctor_ExecutesStaticIndexesOnRepository()
+        {
+            var repositoryMock = new Mock<IRepository>();
+            List<AbstractIndexCreationTask> executedIndexes = null;
+
+            repositoryMock.Setup(r => r.ExecuteIndexes(It.IsAny<List<AbstractIndexCreationTask>>()))
+                          .Callback<List<AbstractIndexCreationTask>>(indexes => executedIndexes = indexes);
+
+            var storage = new RavenStorage(repositoryMock.Object);
+
+            repositoryMock.Verify(r => r.ExecuteIndexes(It.IsAny<List<AbstractIndexCreationTask>>()), Times.Once);
+            Assert.NotNull(executedIndexes);
+            Assert.Equal(2, executedIndexes.Count);
+            Assert.Contains(executedIndexes, i => i is JobQueue_ByQueueAndFetchedAt);
+            Assert.Contains(executedIndexes, i => i is RavenJobs_ByStateAndCreatedAt);
+        }
+    }
+}
